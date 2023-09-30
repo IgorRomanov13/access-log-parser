6e38f7a2f39dd4b573018ee18d97c4c69d886143
Igor Romanov <igor.romanov.mail@gmail.com>
2023-09-30 15:04:34 +0300

    Задание по теме "Циклы" Написан метод, который определяет существует ли файл, путь к которому был указан

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import java.io.File;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int number1 = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int number2 = new Scanner(System.in).nextInt();
-        System.out.println("Сумма: " + (number1 + number2));
-        System.out.println("Разность: " + (number1 - number2));
-        System.out.println("Произведение: " + number1 * number2);
-        System.out.println("Частное: " + (double) number1 / number2);
+        int count = 0;
+        while (true) {
+            System.out.println("Укажите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists == false) {
+                System.out.println("Файл не найден");
+            } else if (isDirectory == true) {
+                System.out.println("Это директория");
+                continue;
+            }
+            if (fileExists == true) {
+                System.out.println("Путь указан верно");
+                count++;
+                System.out.println("Это файл номер " + count);
+            }
+        }
     }
 }
